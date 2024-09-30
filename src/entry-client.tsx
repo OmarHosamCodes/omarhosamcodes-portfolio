dc4de8b587b426b7d688b691eaa2fe83f4b0df07
Omar Hosam <OmarHosamCodes@gmail.com>
2024-09-30 05:05:07 +0300

    simple fixes

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,11 +1,5 @@
 import "@/index.css";
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@/App";
 
-ReactDOM.hydrateRoot(
-	document.getElementById("root") as HTMLElement,
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-);
+ReactDOM.hydrateRoot(document.getElementById("root") as HTMLElement, <App />);
